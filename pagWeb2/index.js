c2f8e103abca475cd9c28c6b03f2e6f9bd1c92b8
Jerónimo Murillo Narvaez <jeronimomurillonarvaez@gmail.com>
2023-07-09 13:22:05 -0500

    Version 1.0.0

---
+++ pagWeb2/index.js
@@ -0,0 +1,17 @@
+//Practice
+
+console.log(19)
+
+const array= []
+
+console.log(array)
+
+
+array.push('Hello World')
+
+console.log(array)
+
+let welcome = "Hello there, my name is Jeronimo Murillo Narvaez"
+let country= "I born in Colombia"
+
+console.log(welcome + country)
